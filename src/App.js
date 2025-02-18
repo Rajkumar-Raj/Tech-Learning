e02d49994e028f34b39f9a33ea0a476df98f49e0
Rajkumar-Raj <rajkumarbecse59@gmail.com>
2025-02-18 11:08:36 -0600

    node changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
-import MyButton from './button.js';
 import './App.css';
-import {useState} from 'react';
 
 function App() {
-
-  const[count, setCount] = useState(0);
-
-  const handleclick = () => setCount(count+1);
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit src/App.js and save to reload. <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -25,11 +17,7 @@ function App() {
         >
           Learn React
         </a>
-count = {count}
-
-        <MyButton count={count} onclick={handleclick} />
       </header>
-      
     </div>
   );
 }
