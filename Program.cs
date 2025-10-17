75d209f8815b59587472ae6c72c65a1d23caa0c2
Rajkumar-Raj <rajkumarbecse59@gmail.com>
2025-10-16 21:19:00 -0500

    404 changes

--- Program.cs
+++ Program.cs
@@ -3,32 +3,32 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TechLearning;
 
-//var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//await builder.Build().RunAsync();
-
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var host = builder.Build();
+await builder.Build().RunAsync();
 
-// 🔸 Handle redirect from 404.html (e.g. /?path=/some/route)
-var uri = new Uri(host.Services.GetRequiredService<NavigationManager>().Uri);
-var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
-var path = query.Get("path");
 
-if (!string.IsNullOrEmpty(path))
-{
-    var nav = host.Services.GetRequiredService<NavigationManager>();
-    nav.NavigateTo(path, forceLoad: false);
-}
+//var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await host.RunAsync();
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//var host = builder.Build();
+
+//// 🔸 Handle redirect from 404.html (e.g. /?path=/some/route)
+//var uri = new Uri(host.Services.GetRequiredService<NavigationManager>().Uri);
+//var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+//var path = query.Get("path");
+
+//if (!string.IsNullOrEmpty(path))
+//{
+//    var nav = host.Services.GetRequiredService<NavigationManager>();
+//    nav.NavigateTo(path, forceLoad: false);
+//}
+
+//await host.RunAsync();
