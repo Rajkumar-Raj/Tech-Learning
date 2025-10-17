27ba3a96c7712903973b87ee3a82a64b86762cd1
Rajkumar-Raj <rajkumarbecse59@gmail.com>
2025-10-16 20:41:31 -0500

    updated 404

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TechLearning;
+
+//var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//await builder.Build().RunAsync();
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +18,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+// 🔸 Handle redirect from 404.html (e.g. /?path=/some/route)
+var uri = new Uri(host.Services.GetRequiredService<NavigationManager>().Uri);
+var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+var path = query.Get("path");
+
+if (!string.IsNullOrEmpty(path))
+{
+    var nav = host.Services.GetRequiredService<NavigationManager>();
+    nav.NavigateTo(path, forceLoad: true);
+}
+
+await host.RunAsync();
